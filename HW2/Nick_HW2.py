2289b710b62e93c6748764980ce59b48f91800ff
Nick Newman <nicknewman01@gmail.com>
2019-10-09 17:23:05 -0400

    adding MSE

--- HW2/Nick_HW2.py
+++ HW2/Nick_HW2.py
@@ -33,10 +33,10 @@ def main():
     '''
     The main function 
     '''
-    print("Start Program --- %s seconds ---" % (round((time.time() - start_time),2)))
+    print("--- Start Program --- %s seconds ---" % (round((time.time() - start_time),2)))
     docs, sentences = get_docs()
-    vector, labels,ngram_array, ngram_label_array, vocab = get_ngrams_vector(docs,sentences,2)
-    train, test = get_embedded_traintest(docs, vector, labels, ngram_array, ngram_label_array, vocab)
+    ngram_array, ngram_label_array, vocab_size = get_ngrams_vector(docs,sentences,2)
+    run_neural_network(ngram_array, ngram_label_array,vocab_size)
     return
 
 
@@ -84,7 +84,7 @@ def get_docs():
         sentences.append(token_tweets)
 
     # print(docs)
-    print("Text Extracted --- %s seconds ---" % (round((time.time() - start_time),2)))
+    print("--- Text Extracted --- %s seconds ---" % (round((time.time() - start_time),2)))
 
    
     return docs, sentences 
@@ -118,13 +118,13 @@ def get_ngrams_vector(docs, sentences, num_grams):
         # get two fake ngrams for each correct one
         for _ in range(2): 
             random_word = random.choice(docs)
-            while random_word == gram[0] or random_word in ngram_dict[gram[0]]:
+            while random_word == gram[0] or random_word in ngram_dict[gram[0]]: #pick words until not duplicate word and not correct bigram
                 random_word = random.choice(docs)
             fakegrams.append((gram[0], random_word))
     
 
 
-    gramvec, labels = [], []
+    gramvec, labels = [], [] 
     for element in ngram_list:
         gramvec.append([element[0],element[1]])
         labels.append([1])
@@ -133,42 +133,42 @@ def get_ngrams_vector(docs, sentences, num_grams):
         labels.append([0])
 
     
-    vocab = set(docs)
-    word_to_ix = {word: i for i, word in enumerate(vocab)}
+    vocab = set(docs) #set vocab as unique words
+    word_to_ix = {word: i for i, word in enumerate(vocab)} #index vocabulary
     
     ngramlabeled = [[gram] + label for gram, label in zip(gramvec,labels)]
-    
-    ngram_values = []
+
+    ngram_values = [] #array of word index for ngrams
     for context, label in ngramlabeled:
         ngram_values.append([word_to_ix[w] for w in context])
     
-    ngram_labels = []
+    ngram_labels = [] # list of labels for ngrams
     for context, label in ngramlabeled:
         ngram_labels.append([label])
         
     ngram_array = np.array(ngram_values)
-    ngram_label_array = np.array(ngram_labels)
+    ngram_label_array = np.array(ngram_labels) #convert to numpy array for use in torch
     
-    print("Grams Created --- %s seconds ---" % (round((time.time() - start_time),2)))
-    return gramvec, labels, ngram_array, ngram_label_array, vocab
+    print("--- Grams Created --- %s seconds ---" % (round((time.time() - start_time),2)))
+    return ngram_array, ngram_label_array, len(vocab)
 
 
 
 
 
-def get_embedded_traintest(docs, gramvec, labels, ngram_array, ngram_label_array, vocab):
+def run_neural_network(ngram_array, ngram_label_array, vocab_size):
     '''
     put grams into embedding format and make test and train set for model
     '''
     
-    BATCH_SIZE = 2500
+    BATCH_SIZE = 150 # 1000 maxes memory for 8GB GPU
 
     X_train, X_test, y_train, y_test = train_test_split(ngram_array, ngram_label_array, test_size=0.2, 
                                                        random_state=1234, shuffle=True, stratify=ngram_label_array)
     X_train, X_valid, y_train, y_valid = train_test_split(X_train, y_train, test_size=0.2, 
                                                        random_state=1234, shuffle=True, stratify=y_train)
     
-    
+    #set datatypes
     X_train = torch.from_numpy(X_train)
     X_train = X_train.long()
     y_train = torch.from_numpy(y_train)
@@ -192,32 +192,34 @@ def get_embedded_traintest(docs, gramvec, labels, ngram_array, ngram_label_array
     
     
     
-    EMBEDDING_DIM = 25
-    CONTEXT_SIZE = 2
-    vocab_size = len(vocab)
-    #word_to_ix = {word: i for i, word in enumerate(vocab)}
+    EMBEDDING_DIM = 25 #25 size embeddings
+    CONTEXT_SIZE = 2 #bigram model
+    HIDDEN_SIZE = 50 # nodes in hidden layer
+
     class NGramLanguageModeler(nn.Module):
     
-        def __init__(self, vocab_size, embedding_dim, context_size, batch_size):
+        def __init__(self, vocab_size, embedding_dim, context_size, batch_size, hidden_size):
             super(NGramLanguageModeler, self).__init__()
             self.embeddings = nn.Embedding(vocab_size*batch_size, embedding_dim)
-            self.linear1 = nn.Linear(context_size * embedding_dim*batch_size, 20)
-            self.linear2 = nn.Linear(20, batch_size)
+            self.linear1 = nn.Linear(context_size * embedding_dim*batch_size, hidden_size)
+            self.linear2 = nn.Linear(hidden_size, batch_size)
             self.out_act = nn.Sigmoid()
     
         def forward(self, inputs):
             embeds = self.embeddings(inputs).view((1, -1))
-            out1 = F.relu(self.linear1(embeds))
+            out1 = self.linear1(embeds)
             out3 = self.linear2(out1)
             yhat = self.out_act(out3)
             return yhat
     
     losses = []
     loss_function = nn.BCELoss()
-    model = NGramLanguageModeler(vocab_size, EMBEDDING_DIM, CONTEXT_SIZE, BATCH_SIZE)
+    # Experimenting with MSE Loss
+    #loss_function = nn.MSELoss()
+    model = NGramLanguageModeler(vocab_size, EMBEDDING_DIM, CONTEXT_SIZE, BATCH_SIZE, HIDDEN_SIZE)
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     model.to(device)
-    optimizer = optim.SGD(model.parameters(), lr=0.00001)
+    optimizer = optim.SGD(model.parameters(), lr=0.01)
 
     yhat_list = []
     context_list = []
@@ -228,24 +230,23 @@ def get_embedded_traintest(docs, gramvec, labels, ngram_array, ngram_label_array
     train_maxiter = X_train.size(0)//BATCH_SIZE
     valid_maxiter = X_valid.size(0)//BATCH_SIZE
     
-    
-    for epoch in range(5):
+    accuracy = 0
+    print("Start Training --- %s seconds ---" % (round((time.time() - start_time),2)))
+    for epoch in range(1):
         iteration = 0
-        running_loss = 0.0
-        print('Epoch: {}'.format(epoch+1))  
+        running_loss = 0.0 
+        print('--- Epoch: {} | Current Validation Accuracy: {} ---'.format(epoch+1, accuracy)) 
         for i, (context, label) in enumerate(trainloader):
             if i+1 < train_maxiter:
-
                 # zero out the gradients from the old instance
                 optimizer.zero_grad()
                 # Run the forward pass and get predicted output
                 context = context.to(device)
                 label = label.to(device)
-                yhat = model(context)
+                yhat = model.forward(context)
                 yhat = yhat.view(-1,1)
                 yhat_list.append(yhat)
                 context_list.append(context)
-
 
                 # Compute Binary Cross-Entropy
                 labels.append(label)
@@ -257,27 +258,42 @@ def get_embedded_traintest(docs, gramvec, labels, ngram_array, ngram_label_array
                 iteration += 1
                 # Get the Python number from a 1-element Tensor by calling tensor.item()
                 running_loss += loss.item()
-                
-                # Get the accuracy on the validation set
-                if not i % 50:
-                    total = 0
-                    num_correct = 0
-                    for a, (context, label) in enumerate(validloader):
-                        if a+1 < valid_maxiter:
-                            context = context.to(device)
-                            label = label.to(device)
-                            yhat = model(context)
-                            yhat = yhat.view(-1,1)
-                            predictions = (yhat > 0.5)
-                            total += label.nelement()
-                            num_correct += torch.sum(torch.eq(predictions, label.byte())).item()
-                    accuracy = num_correct/total*100
-                    print('Validation Accuracy {}'.format(accuracy))
-        
-                print('Epoch: {}, Iteration: {}, loss: {} running loss: {}'.format(epoch,iteration, loss.item(), running_loss/train_maxiter))
+                # print('Epoch: {}, Iteration: {}, loss: {} running loss: {}'.format(epoch,iteration, loss.item(), running_loss/train_maxiter))
         
             losses.append(loss.item())
-    
+    # Get the accuracy on the validation set for each epoch
+        with torch.no_grad():
+            total = 0
+            num_correct = 0
+            for a, (context, label) in enumerate(validloader):
+                if a+1 < valid_maxiter:
+                    context = context.to(device)
+                    label = label.to(device)
+                    yhat = model.forward(context)
+                    yhat = yhat.view(-1,1)
+                    predictions = (yhat > 0.5)
+                    total += label.nelement()
+                    num_correct += torch.sum(torch.eq(predictions, label.bool())).item()
+            accuracy = num_correct/total*100
+            # print('Validation Accuracy {}'.format(accuracy))
+
+    print("Training Complete --- %s seconds ---" % (round((time.time() - start_time),2)))
+    # Get the accuracy on the test set after training complete
+    with torch.no_grad():
+        test_maxiter = X_test.size(0)//BATCH_SIZE
+        total = 0
+        num_correct = 0
+        for a, (context, label) in enumerate(testloader):
+            if a+1 < test_maxiter:
+                context = context.to(device)
+                label = label.to(device)
+                yhat = model.forward(context)
+                yhat = yhat.view(-1,1)
+                predictions = (yhat > 0.5)
+                total += label.nelement()
+                num_correct += torch.sum(torch.eq(predictions, label.bool())).item()
+        accuracy = num_correct/total*100
+        print('Test Accuracy: {} %'.format(round(accuracy,5)))
     return
 if __name__ == "__main__":
     main()
