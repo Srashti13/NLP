65a541b5a4d26e69e742d59fb634bfd2bc0aaa6a
william ermlcik <ermlickw@vt.edu>
2019-10-31 23:06:52 -0400

    almost read for RNN

--- HW3/HW3.py
+++ HW3/HW3.py
@@ -9,14 +9,17 @@ Command to run the file: python HW3.py
 import re
 from collections import defaultdict
 import numpy as np
+import time
 
+start_time = time.time()
 
 def main():
-    train_vocab, train_sentences = get_sentences(r"conll2003\train.txt")
-    valid_vocab, valid_sentences = get_sentences(r"conll2003\valid.txt")
-    test_vocab, test_sentences = get_sentences(r"conll2003\test.txt")
+    print("--- Start Program --- %s seconds ---" % (round((time.time() - start_time),2)))
+    train_vocab, train_sentences, totalpadlength = get_sentences(r"conll2003\train.txt")
+    valid_vocab, valid_sentences, _ = get_sentences(r"conll2003\valid.txt")
+    test_vocab, test_sentences, _ = get_sentences(r"conll2003\test.txt")
     vocab = dict_combination(train_vocab, valid_vocab, test_vocab)
-    get_context_vectors(vocab, train_sentences, valid_sentences, test_sentences)
+    vectorized_data = get_context_vectors(vocab, train_sentences, valid_sentences, test_sentences)
 
     return
 
@@ -33,14 +36,23 @@ def dict_combination(dictone,dicttwo,dictthree):
 
 #%%
 def get_sentences(docs):
+
+    # tokenizing: lowercasing all words that have some, but not all, uppercase
+    def lower_repl(match):
+        return match.group().lower()
+    def lowercase_text(txt):
+        txt = re.sub('([A-Z]+[a-z]+)',lower_repl,txt) #lowercase words that start with captial    
+        return txt   
+
+
     vocab = defaultdict(list)
     doc = []
     with open(docs) as f:
         for word in f.read().splitlines():
             a = word.split(" ")
             if len(a)>1:
-                vocab[a[0]].append(a[3])
-                doc.append([a[0],a[3]])
+                vocab[lowercase_text(a[0])].append(a[3])
+                doc.append([lowercase_text(a[0]),a[3]])
             else: 
                 doc.append(a[0])
     doc.insert(0,'')
@@ -48,18 +60,7 @@ def get_sentences(docs):
     # retaining the unique tags for each vocab word
     for k,v in vocab.items():
         vocab[k] = (list(set(v)))
-        
-    # lowercasing all words that have some, but not all, uppercase
-    def lower_repl(match):
-        return match.group().lower()
-    def lowercase_text(txt):
-        txt = re.sub('([A-Z]+[a-z]+)',lower_repl,txt) #lowercase words that start with captial    
-        return txt        
 
-    for word in doc:
-        if word:
-            word[0] = lowercase_text(word[0])
-            
     # getting the indices of the end of each sentence
     sentence_ends = []
     for i, word in enumerate(doc):
@@ -81,55 +82,51 @@ def get_sentences(docs):
         sentence.extend(['0','<pad>'] for i in range(max_sent_len-len(sentence)))
     # This is the code to read the embeddings
     vocab['0'] = '<pad>'
-    return vocab, sentences
+    print("--- Text Extracted --- %s seconds ---" % (round((time.time() - start_time),2)))
+    return vocab, sentences, max_sent_len
 
 
-def get_context_vectors(vocab, train_questions, train_labels, test_questions):
+def get_context_vectors(vocab, train_sentences, valid_sentences, test_sentences):
     '''
-    Construct your n-grams: Create positive n-gram samples by collecting all pairs of adjacent
-    tokens. Create 2 negative samples for each positive sample by keeping the first word the same
-    as the positive sample, but randomly sampling the rest of the corpus for the second word. The
-    second word can be any word in the corpus except for the first word itself. 
-    
-    This functions takes the docs and tokenized sentences and creates the numpyarrays needed for the neural network.
-    --creates 2 fake grams for every real gram 
+    convert to numpy vectors 
     '''
-    import operator
-    word_to_ix = {word: i+1 for i, word in enumerate(vocab)} #index vocabulary
-    word_to_ix['XXPADXX'] = 0 #set up padding
-    vocab.append('XXPADXX')
+    word_to_ix = {word: i for i, word in enumerate(vocab)} #index vocabulary
 
-    train_context_values = [] #array of word index for context 
-    for context in train_questions.values:
-        train_context_values.append([word_to_ix[w] for w in context])
+    train_context_values = [] #array of word index for context
+    train_label_values = [] 
+    for sentence in train_sentences:
+        train_context_values.append([word_to_ix[w[0]] for w in sentence])
+        train_label_values.append([w[1] for w in sentence])
 
-    test_context_values = [] #array of word index for context 
-    for context in test_questions.values:
-        test_context_values.append([word_to_ix[w] for w in context])
-    
-    train_context_labels = [] # list of labels for context
-    for label in train_labels.values:
-        train_context_labels.append([label])
+    valid_context_values = [] #array of word index for context
+    valid_label_values = [] 
+    for sentence in valid_sentences:
+        valid_context_values.append([word_to_ix[w[0]] for w in sentence])
+        valid_label_values.append([w[1] for w in sentence])
+
+    test_context_values = [] #array of word index for context
+    test_label_values = [] 
+    for sentence in test_sentences:
+        test_context_values.append([word_to_ix[w[0]] for w in sentence])
+        test_label_values.append([w[1] for w in sentence])
         
     #convert to numpy array for use in torch  -- padding with index 0 for padding.... Should change to a random word...
-    totalpadlength = max(max(map(len, train_context_values)),max(map(len, test_context_values))) #the longest question 
-    train_context_array = np.array([xi+[0]*(totalpadlength-len(xi)) for xi in train_context_values]) #needed because without padding we are lost 
-    test_context_array = np.array([xi+[0]*(totalpadlength-len(xi)) for xi in test_context_values]) #needed because without padding we are lost 
-    train_context_label_array = np.array(train_context_labels) 
+    train_context_array = np.array(train_context_values)
+    valid_context_array = np.array(valid_context_values) 
+    test_context_array = np.array(test_context_values)
+    train_context_label_array = np.array(train_label_values)
+    valid_context_label_array = np.array(valid_label_values)
+    test_context_label_array = np.array(test_label_values) 
+    arrays_and_labels = {"train_context_array":train_context_array,
+                        "train_context_label_array":train_context_label_array,
+                        "valid_context_array":valid_context_array,
+                        "valid_context_label_array":valid_context_label_array,
+                        "test_context_array":test_context_array,
+                        "test_context_array":test_context_array,
+                        "test_context_label_array":test_context_label_array}
 
-    #used to back convert to words from index
-    ix_to_word = {} 
-    for key, value in word_to_ix.items(): 
-        if value in ix_to_word: 
-            ix_to_word[value].append(key) 
-            print(value)
-            print(key)
-            print(ix_to_word[value])
-        else: 
-            ix_to_word[value]=[key] 
-
-    print("--- Grams Created --- %s seconds ---" % (round((time.time() - start_time),2)))
-    return train_context_array, train_context_label_array, test_context_array, totalpadlength, ix_to_word, vocab
+    print("--- Arrays Created --- %s seconds ---" % (round((time.time() - start_time),2)))
+    return arrays_and_labels
 # from gensim.models.keyedvectors import KeyedVectors
 # model = KeyedVectors.load_word2vec_format('D:\GoogleNews-vectors-negative300.bin', binary=True)
 # model.save_word2vec_format('D:\GoogleNews-vectors-negative300.txt', binary=False)
