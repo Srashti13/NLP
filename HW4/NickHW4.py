b3993879b851e6a65c33830e68c12e90ae8d6137
william ermlcik <ermlickw@vt.edu>
2019-11-24 12:02:01 -0500

    preprocessing

--- HW4/NickHW4.py
+++ HW4/NickHW4.py
@@ -11,66 +11,84 @@ import gc
 from sklearn.metrics import accuracy_score
 import pandas as pd
 #%%
+def main():
+    train, test = preprocessdata()
+    
+    return
 
-train = r"C:\Users\nickn\Desktop\Grad School\AIT_726\Homework4\train-set.txt"
-vocab = defaultdict(list)
-doc = []
-sentences = []
-arguments = []
-with open(train) as f:
-    sentence = []
-    for line in f.read().splitlines():
-        a = re.split(" +", line)
-        if a[0] == " " or a[0] == "":
-            doc.append(['<BREAK>']*14)
-        else:
-            doc.append(a)
-train_df = pd.DataFrame(doc)
-train_df.columns = ["word","pos","full_tree","ner","clauses","prop1","prop2","prop3","prop4","prop5","prop6","prop7","prop8","prop9"]
-train_df = train_df.drop(columns=["full_tree","ner","clauses","pos"], axis=1)
-         
-            
-#%%
-total_train = []
-for i in range(len(train_df.columns)-1):
-    total_train.append((train_df[['word',train_df.columns[i+1]]].values.tolist()))
+def preprocessdata():
+    def BIOconversion(df):
+        prop_values = []
+        prop_value = "O"
+        for i in range(len(df)):
+            if df.iloc[i] == '<BREAK>':
+                prop_values.append(df[i])
+            elif df.iloc[i] == 'None':
+                prop_values.append(df[i])
+            elif df.iloc[i][0] == '(' and df.iloc[i][-1] == ')':
+                prop_values.append(df.iloc[i][1:-2])
+            elif df.iloc[i][0] == '(':
+                prop_value = df.iloc[i][1:-1]
+                prop_values.append("B-" + df.iloc[i][1:-1])
+            elif df.iloc[i][-1] == ')':
+                prop_values.append("I-" + prop_value)
+                prop_value = ""
+            elif df.iloc[i][-1] == '*' and prop_value == "":
+                prop_values.append("O")
+            elif df.iloc[i][-1] == '*' and (prop_value != "" and prop_value !="O"):
+                prop_values.append("I-" + prop_value)
+            else:
+                prop_values.append(prop_value)     
+        return prop_values
 
-total_train = [word_pair for sentence in total_train for word_pair in sentence]
-train_df = pd.DataFrame(total_train, columns=['word','prop'])
+    train = r"data.wsj/train-set.txt"
+    vocab = defaultdict(list)
+    doc = []
+    sentences = []
+    arguments = []
+    with open(train) as f:
+        sentence = []
+        for line in f.read().splitlines():
+            a = re.split(" +", line)
+            if a[0] == " " or a[0] == "":
+                doc.append(['<BREAK>']*14)
+            else:
+                doc.append(a)
 
-#%%
+    train_df = pd.DataFrame(doc)
+    train_df.columns = ["word","pos","full_tree","ner","targetverb","prop1","prop2","prop3","prop4","prop5","prop6","prop7","prop8","prop9"]
+    # print(train_df['prop1'].head(30))
+    train_df.replace("", np.nan, inplace=True)
+    train_df = train_df.fillna("None")
+    for col in train_df.columns[5:]:
+        train_df[col] = BIOconversion(train_df[col])
+    # print(train_df.head(50))
 
-train_df = train_df[train_df['prop'].isna()==False]
-train_df = train_df[train_df['prop']!=""]
-train_df = train_df = train_df.reset_index(drop=True)
-        
-#%%
-# removing rows where there are two duplicates in a row (usually <BREAK> items)
-train_df = train_df.loc[(train_df.shift() != train_df).all(axis=1)]
-train_df = train_df = train_df.reset_index(drop=True)
+    #preprocess test 
+    test = r"data.wsj/test-set.txt"
+    vocab = defaultdict(list)
+    doc = []
+    sentences = []
+    arguments = []
+    with open(test) as f:
+        sentence = []
+        for line in f.read().splitlines():
+            a = re.split(" +", line)
+            if a[0] == " " or a[0] == "":
+                doc.append(['<BREAK>']*14)
+            else:
+                doc.append(a)
 
+    test_df = pd.DataFrame(doc)
+    test_df.columns = ["word","pos","full_tree","ner","targetverb","prop1","prop2","prop3","prop4","prop5","prop6","prop7","prop8","prop9"]
+    test_df.replace("", np.nan, inplace=True)
+    test_df = test_df.fillna("None")
+    for col in test_df.columns[5:]:
+        test_df[col] = BIOconversion(test_df[col])
+    comb = pd.concat([test_df.iloc[:,5],train_df.iloc[:,5]])
+    print(set(comb)) #set of argument tags 
 
-#%%
-# labeling the dataframe with BIO structure
-prop_values = []
-for i in range(len(train_df)):
-    if train_df['prop'].iloc[i] == '<BREAK>':
-        prop_values.append(train_df['prop'][i])
-    elif train_df['prop'].iloc[i][0] == '(' and train_df['prop'].iloc[i][-1] == ')':
-        prop_values.append(train_df['prop'].iloc[i][1:-2])
-    elif train_df['prop'].iloc[i][0] == '(':
-        prop_value = train_df['prop'].iloc[i][1:-1]
-        prop_values.append("B-" + train_df['prop'].iloc[i][1:-1])
-    elif train_df['prop'].iloc[i][-1] == ')':
-        prop_values.append("I-" + prop_value)
-        prop_value = ""
-    elif train_df['prop'].iloc[i][-1] == '*' and prop_value == "":
-        prop_values.append("O")
-    elif train_df['prop'].iloc[i][-1] == '*' and prop_value != "":
-        prop_values.append("I-" + prop_value)
-        
-train_df['prop_values'] = prop_values
-#%%
-## making the target Verb (predicate) values apparent
-train_df['target'] = train_df['prop_values'] == 'V'
-train_df['target'] = train_df['target'].astype(int)
+    return train_df, test_df
+
+if __name__ == "__main__":
+    main()
