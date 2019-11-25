4a910248b539ec96eb0638895be2b8f8db6e24d8
william ermlcik <ermlickw@vt.edu>
2019-11-24 20:52:56 -0500

    validation set created. need to configure RNN next

--- HW4/BillyHW4.py
+++ HW4/BillyHW4.py
@@ -11,14 +11,20 @@ import gc
 from sklearn.metrics import accuracy_score
 import pandas as pd
 import time
+import math
+from sklearn.model_selection import train_test_split
 
 start_time = time.time()
 
 def main():
     print("--- Start Program --- %s seconds ---" % (round((time.time() - start_time),2)))
     train, test = preprocessdata()
-    vectorizeddata, indicies, revindicies, vocab = getvectors(train, test)
-    weights_matrix_torch = build_weights_matrix(vocab, "GoogleNews-vectors-negative300.txt", embedding_dim=300)
+    vectorized_data, indicies, revindicies, vocab = getvectors(train, test)
+    # weights_matrix_torch = build_weights_matrix(vocab, "GoogleNews-vectors-negative300.txt", embedding_dim=300)
+
+    run_RNN(vectorized_data, vocab, indicies, hidden_dim=256,
+            bidirectional=True, pretrained_embeddings_status=False, RNNTYPE="LSTM")
+
     return
 
 def preprocessdata():
@@ -186,8 +192,15 @@ def getvectors(train,test):
     y=[xi+[[xi[0][0],xi[0][1],word_to_ix['<pad>'],word_to_ix['<pad>'],pos_to_ix['<pad>'],ner_to_ix['<pad>'],prop_to_ix['<pad>']]]*(length-len(xi)) for xi in train_sentences]
     y=np.array(y)
     train_sentences = y.transpose(0,2,1)
-    train_labels = train_sentences[:,-1,:]
-    train_sentences = train_sentences[:,:-2,:]
+    train_labelsfull = train_sentences[:,-1,:]
+    train_sentencesfull = train_sentences[:,:-2,:]
+    splitpoint = int(round(train_sentencesfull.shape[0]*.8))
+    print(train_labelsfull.shape)
+    train_sentences = train_sentencesfull[:splitpoint,:,:]
+    valid_sentences = train_sentencesfull[splitpoint:,:,:]
+    train_labels = train_labelsfull[:splitpoint,:]
+    valid_labels = train_labelsfull[splitpoint:,:]
+
 
     #same for test
     combos = []
@@ -214,8 +227,11 @@ def getvectors(train,test):
     vectorizeddata = defaultdict()
     vectorizeddata = {'train_sents': train_sentences,
                       'train_lab': train_labels,
+                      'valid_sents': valid_sentences,
+                      'valid_lab': valid_labels,
                       'test_sents':test_sentences,
                       'test_lab':test_labels}
+
     indicies = defaultdict()
     indicies = {'word_to_ix': word_to_ix,
                 'pos_to_ix':pos_to_ix,
@@ -307,6 +323,223 @@ def build_weights_matrix(vocab, embedding_file, embedding_dim):
             
     print("{:.2f}% ({}/{}) of the vocabulary were in the pre-trained embedding.".format((words_found/len(vocab))*100,words_found,len(vocab)))
     return torch.from_numpy(weights_matrix)
+
+def run_RNN(vectorized_data, vocab, revindicies, hidden_dim, weights_matrix_torch=[], bidirectional=False,pretrained_embeddings_status=False, 
+            RNNTYPE="RNN"):
+    '''
+    This function uses pretrained embeddings loaded from a file to build an RNN of various types based on the parameters
+    bidirectional will make the network bidirections, pretrained_embeddings_status=False will have the model learn its own embeddings
+    '''
+
+    def format_tensors(vectorized_data, dataset_type,num_mini_batches):
+        '''
+        helper function to format numpy vectors to the correct type, also determines the batch size for train, valid, and test sets
+        based on minibatch size
+        '''
+        X = torch.from_numpy(vectorized_data[dataset_type+'_sents'])
+        X = X.long()
+        batch_size = math.ceil(X.size(0)/num_mini_batches) # 200 mini-batches per epoch
+        y = torch.from_numpy(vectorized_data[dataset_type+'_lab'])
+        y = y.long()
+        tensordata = data_utils.TensorDataset(X,y)
+        loader = data_utils.DataLoader(tensordata, batch_size=batch_size,shuffle=False)
+        return loader
+
+    # building data loaders
+    NUM_MINI_BATCHES = 200 #not 2000 for time purposes
+    trainloader = format_tensors(vectorized_data,'train',NUM_MINI_BATCHES)
+    validloader = format_tensors(vectorized_data,'valid',NUM_MINI_BATCHES)
+    testloader = format_tensors(vectorized_data,'test',NUM_MINI_BATCHES)
+    
+    def create_emb_layer(weights_matrix, non_trainable=False):
+        '''
+        creates torch embeddings layer from matrix
+        '''
+        num_embeddings, embedding_dim = weights_matrix.size()
+        emb_layer = nn.Embedding(num_embeddings, embedding_dim)
+        emb_layer.load_state_dict({'weight':weights_matrix})
+        if non_trainable:
+            emb_layer.weight.requires_grad = False
+        return emb_layer, num_embeddings, embedding_dim
+    
+    class RNNmodel(nn.Module):
+        def __init__(self, weights_matrix, hidden_size, bidirectional, pre_trained=True):
+            super(RNNmodel, self).__init__()
+            if bidirectional:
+                num_directions = 2
+            else:
+                num_directions = 1
+            self.embedding, num_embeddings, embedding_dim = create_emb_layer(weights_matrix, pre_trained)
+            if RNNTYPE=="LSTM":
+                print("----Using LSTM-----")
+                self.rnn = nn.LSTM(embedding_dim, hidden_size=hidden_size, batch_first=True,
+                                    bidirectional=bidirectional)
+            elif RNNTYPE=="GRU":
+                print("----Using GRU-----")
+                self.rnn = nn.GRU(embedding_dim, hidden_size=hidden_size, batch_first=True,
+                                    bidirectional=bidirectional)
+            else:
+                print("----Using RNN-----")
+                self.rnn = nn.RNN(embedding_dim, hidden_size=hidden_size, batch_first=True,
+                                    bidirectional=bidirectional)
+            self.fc = nn.Linear(hidden_size*num_directions,10)
+            
+        def forward(self, inputs):
+            embeds = self.embedding(inputs) # dim: batch_size x batch_max_len x embedding_dim
+            out, _ = self.rnn(embeds) # dim: batch_size x batch_max_len x lstm_hidden_dim*directions 
+            out = out.contiguous().view(-1, out.shape[2]) # dim: batch_size*batch_max_len x lstm_hidden_dim
+            yhats = self.fc(out) # dim: batch_size*batch_max_len x num_tags                       #https://cs230-stanford.github.io/pytorch-nlp.html
+            return yhats #CrossEntropy in pytorch takes care of softmax here
+
+
+    losses = []
+    def class_proportional_weights(train_labels):
+        '''
+        helper function to scale weights of classes in loss function based on their sampled proportions
+        # This custom loss function is defined to reduce the effect of class imbalance.
+        # Since there are so many samples labeled as "O", this allows the RNN to not 
+        # be weighted too heavily in that area.
+        '''
+        weights = []
+        flat_train_labels = [item for sublist in train_labels for item in sublist]
+        for lab in range(1,10):
+            weights.append(1-(flat_train_labels.count(lab)/(len(flat_train_labels)-flat_train_labels.count(0)))) #proportional to number without tags
+        weights.insert(0,0) #zero padding values weight
+        return weights
+    weights = class_proportional_weights(vectorized_data['train_context_label_array'].tolist()) #zero out pads and reduce weights given to "O" objects in loss function
+    class_weights = torch.FloatTensor(weights).cuda()
+    criterion = nn.CrossEntropyLoss(weight=class_weights)
+
+    #initalize model parameters and variables
+    model = RNNmodel(weights_matrix_torch, hidden_dim, bidirectional, pretrained_embeddings_status)
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu") #run on gpu if available...
+
+    model.to(device)
+    optimizer = optim.Adam(model.parameters(), lr=0.005) #learning rate set to 0.005 to converse faster -- change to 0.00001 if desired
+    torch.backends.cudnn.benchmark = True #memory
+    torch.backends.cudnn.enabled = True #memory https://blog.paperspace.com/pytorch-memory-multi-gpu-debugging/
+    
+    metric_list = []
+    best_metric = 0 
+    print("Start Training --- %s seconds ---" % (round((time.time() - start_time),2)))
+    for epoch in range(50): 
+        iteration = 0
+        running_loss = 0.0 
+        for i, (context, label) in enumerate(trainloader):
+            # zero out the gradients from the old instance
+            optimizer.zero_grad()
+            # Run the forward pass and get predicted output
+            label = label.contiguous().view(-1) # convert to length batch_size*seq_len
+            context = context.to(device)
+            label = label.to(device)
+            yhat = model.forward(context) #required dimensions for batching
+            # Compute Binary Cross-Entropy
+            loss = criterion(yhat, label)
+            #clear memory 
+            del context, label #memory 
+            # Do the backward pass and update the gradient
+            loss.backward()
+            optimizer.step()
+            iteration += 1
+            # Get the Python number from a 1-element Tensor by calling tensor.item()
+            running_loss += float(loss.item())
+            torch.cuda.empty_cache() #memory
+        losses.append(float(loss.item()))
+        del loss #memory 
+        gc.collect() #memory
+        torch.cuda.empty_cache() #memory
+
+    # Get the accuracy on the validation set for each epoch
+        with torch.no_grad():
+            predictionsfull = []
+            labelsfull = []
+            for a, (context, label) in enumerate(validloader):
+                label = label.contiguous().view(-1) # convert to length batch_size*seq_len
+                context = context.to(device)
+                label = label.to(device)
+                yhats = model.forward(context)
+                index = yhats.max(1)[1] #index position of max value
+                prediction = index.int().tolist()
+                predictionsfull.extend(prediction)
+                labelsfull.extend(label.int().tolist())
+                del context, label, prediction #memory
+            gc.collect()#memory
+            torch.cuda.empty_cache()#memory
+            # gpu_usage()
+
+            # remove pads and "O" and do acc calculation:
+            padindicies = [i for i, x in enumerate(labelsfull) if x == 0 or x==1] 
+            for index in sorted(padindicies, reverse=True):
+                del labelsfull[index]
+                del predictionsfull[index]
+            metricscore = accuracy_score(labelsfull, predictionsfull) #not sure if they are using macro or micro in competition
+            metric_list.append(metricscore)
+        print('--- Epoch: {} | Validation Accuracy (non-O): {} ---'.format(epoch+1, metric_list[-1])) 
+
+        if metric_list[-1] > best_metric: #save if it improves validation accuracy 
+            best_metric = metric_list[-1]
+            bestmodelparams = torch.save(model.state_dict(), 'train_valid_best.pth') #save best model
+        #early stopping condition
+        if epoch+1 >= 5: #start looking to stop after this many epochs
+            if metric_list[-1] < min(metric_list[-5:-1]): #if accuracy lower than lowest of last 10 values
+                print('...Stopping Early...')
+                break
+
+    print("Training Complete --- %s seconds ---" % (round((time.time() - start_time),2)))
+        # Get the accuracy on the validation set for each epoch
+    model.load_state_dict(torch.load('train_valid_best.pth')) #load best model
+    with torch.no_grad():
+        predictionsfull = []
+        labelsfull = []
+        contextfull = []
+        for a, (context, label) in enumerate(testloader):
+            label = label.contiguous().view(-1) # convert to length batch_size*seq_len
+            labelsfull.extend(label.int().tolist()) #saving for pad removal and pack conversion
+            contextfull.extend(context.int().tolist())
+            context = context.to(device)
+            label = label.to(device)
+            yhats = model.forward(context)
+            index = yhats.max(1)[1] #index position of max value
+            prediction = index.int().tolist()
+            predictionsfull.extend(prediction) #saving for pad removal and pack conversion
+            del context, label, prediction #memory
+        gc.collect()#memory
+        torch.cuda.empty_cache()#memory
+
+
+        #converting to flat list
+        contextfull = [item for sublist in contextfull for item in sublist]
+        print("--- Removing Pads and Finding Test Accuracy --- %s seconds ---" % (round((time.time() - start_time),2)))
+        #remove pads and do acc calculation:
+        padindicies = [i for i, x in enumerate(labelsfull) if x == 0]
+        for index in sorted(padindicies, reverse=True):
+            del labelsfull[index]
+            del predictionsfull[index]
+            del contextfull[index]
+        metricscore = accuracy_score(labelsfull,predictionsfull) #not sure if they are using macro or micro in competition
+    print('--- Test Accuracy: {} ---'.format(metricscore))
+    print("--- Formatting Results for conlleval.py Official Evaluation --- %s seconds ---" % (round((time.time() - start_time),2)))
+    formattedcontexts = []
+    formattedlabels = []
+    formattedpredictions = []
+    for element in labelsfull: #convert to real words and labels
+        formattedlabels.extend(labelindex[element])
+    for element in predictionsfull:
+        if element == 0:
+            element = 1 #remove stray <pad> predictions
+        formattedpredictions.extend(labelindex[element])
+    for element in contextfull:
+        formattedcontexts.extend(wordindex[element])
+    #write to file
+    fname = 'results/{}--bidir={}--hidden_size={}--pretrain={}--results.txt'.format(RNNTYPE,bidirectional,hidden_dim,pretrained_embeddings_status)
+    if os.path.exists(fname):
+        os.remove(fname)
+    f = open(fname,'w')
+    for (i,element) in enumerate(labelsfull):
+        f.write(formattedcontexts[i] + ' ' + formattedlabels[i] + ' ' + formattedpredictions[i] + '\n')
+    f.close()
+    print('--- {}--bidir={}--hidden_size={}--pretrain={}--results ---'.format(RNNTYPE,bidirectional,hidden_dim,pretrained_embeddings_status))
+    evaluate_conll_file(open(fname,'r')) #evaluate using conll script
 
 if __name__ == "__main__":
     main()
