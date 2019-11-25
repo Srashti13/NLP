fe89e99ed6d3f8f1d8d1826c7447b8cafcb51e33
william ermlcik <ermlickw@vt.edu>
2019-11-24 20:32:25 -0500

    preprocessing complete moving to embedding

--- HW4/BillyHW4.py
+++ HW4/BillyHW4.py
@@ -10,10 +10,15 @@ import torch.nn as nn
 import gc
 from sklearn.metrics import accuracy_score
 import pandas as pd
-#%%
+import time
+
+start_time = time.time()
+
 def main():
+    print("--- Start Program --- %s seconds ---" % (round((time.time() - start_time),2)))
     train, test = preprocessdata()
-    vectorizeddata, indicies = getvectors(train, test)
+    vectorizeddata, indicies, revindicies, vocab = getvectors(train, test)
+    weights_matrix_torch = build_weights_matrix(vocab, "GoogleNews-vectors-negative300.txt", embedding_dim=300)
     return
 
 def preprocessdata():
@@ -46,7 +51,7 @@ def preprocessdata():
             else:
                 prop_values.append(prop_value)     
         return prop_values
-
+    print("--- Extracting Train --- %s seconds ---" % (round((time.time() - start_time),2)))
     train = r"data.wsj/train-set-small.txt"
     vocab = defaultdict(list)
     doc = []
@@ -69,7 +74,7 @@ def preprocessdata():
         train_df[col] = BIOconversion(train_df[col]) #all props to bio notation
     train_df['ner'] = BIOconversion(train_df['ner']) #all ner to bio notation
     # print(train_df.head(50))
-
+    print("--- Extracting Test --- %s seconds ---" % (round((time.time() - start_time),2)))
     #preprocess test 
     test = r"data.wsj/test-set-small.txt"
     vocab = defaultdict(list)
@@ -95,7 +100,7 @@ def preprocessdata():
     # print(test_df.iloc[12850:12890,:])
     # print(train_df.head(50))
     #drop training and testing sentence which have no arguments
-
+    print("--- Splitting Train into Sentences --- %s seconds ---" % (round((time.time() - start_time),2)))
     #split into seperate sentences for each target verb
     trainsplit_df = pd.DataFrame()
     for s in train_df.sentence.unique():
@@ -114,7 +119,7 @@ def preprocessdata():
                         trainsplit_df = pd.concat([trainsplit_df,df2],axis=0, sort=False)
                         number +=1
     # print(trainsplit_df)
-
+    print("--- Splitting Test into Sentences --- %s seconds ---" % (round((time.time() - start_time),2)))
     testsplit_df = pd.DataFrame()
     for s in test_df.sentence.unique():
         number = 0
@@ -141,10 +146,10 @@ def getvectors(train,test):
     train =train.drop(['full_tree'],axis=1)
     test =test.drop(['full_tree'],axis=1)
     full = pd.concat([train,test])
-    
+    print("--- Vectorizing --- %s seconds ---" % (round((time.time() - start_time),2)))
     #index
-    vocab = full['word'].unique().tolist() + ['<pad>']
-    word_to_ix = {word: i for i, word in enumerate(set(vocab))} #index vocabulary
+    wordvocab = full['word'].unique().tolist() + ['<pad>']
+    word_to_ix = {word: i for i, word in enumerate(set(wordvocab))} #index vocabulary
     vocab = full['pos'].unique().tolist() + ['<pad>']
     pos_to_ix = {pos: i for i, pos in enumerate(set(vocab))} #pos vocabulary
     vocab = full['ner'].unique().tolist() + ['<pad>']
@@ -164,11 +169,7 @@ def getvectors(train,test):
     
     train['prop'] = train['prop'].apply(lambda x: prop_to_ix[x])
     test['prop'] = test['prop'].apply(lambda x: prop_to_ix[x])
-    # print(train.head(50))
-    # print(train.transpose().head(50))
-    # train = train.transpose()
-    #convert to numpy arrays and pad
-    # print(train.values.tolist())
+
     combos = []
     train_sentences = []
     for word in train.values.tolist():
@@ -184,8 +185,128 @@ def getvectors(train,test):
     length = max(map(len, train_sentences))
     y=[xi+[[xi[0][0],xi[0][1],word_to_ix['<pad>'],word_to_ix['<pad>'],pos_to_ix['<pad>'],ner_to_ix['<pad>'],prop_to_ix['<pad>']]]*(length-len(xi)) for xi in train_sentences]
     y=np.array(y)
-    print(y.shape)
-    print(y.transpose(0,2,1))
+    train_sentences = y.transpose(0,2,1)
+    train_labels = train_sentences[:,-1,:]
+    train_sentences = train_sentences[:,:-2,:]
+
+    #same for test
+    combos = []
+    test_sentences = []
+    for word in test.values.tolist():
+        # print(combos)
+        # print((word[0],word[1]))
+        # print(train_sentences)
+        if (word[0],word[1]) not in set(combos):
+            test_sentences.append([word])
+            combos.append((word[0],word[1]))
+        else:
+            test_sentences[-1].extend([word])
+    # y=np.array([np.array(xi) for xi in train_sentences])
+    length = max(map(len, test_sentences))
+    y=[xi+[[xi[0][0],xi[0][1],word_to_ix['<pad>'],word_to_ix['<pad>'],pos_to_ix['<pad>'],ner_to_ix['<pad>'],prop_to_ix['<pad>']]]*(length-len(xi)) for xi in test_sentences]
+    y=np.array(y)
+    # print(y.shape)
+    # print(y.transpose(0,2,1))
+    test_sentences = y.transpose(0,2,1)
+    test_labels = test_sentences[:,-1,:]
+    test_sentences = test_sentences[:,:-2,:]
+    print("--- Vectorizing Complete --- %s seconds ---" % (round((time.time() - start_time),2)))
+    vectorizeddata = defaultdict()
+    vectorizeddata = {'train_sents': train_sentences,
+                      'train_lab': train_labels,
+                      'test_sents':test_sentences,
+                      'test_lab':test_labels}
+    indicies = defaultdict()
+    indicies = {'word_to_ix': word_to_ix,
+                'pos_to_ix':pos_to_ix,
+                'ner_to_ix':ner_to_ix,
+                'prop_to_ix':prop_to_ix}
+
+
+    #back conversions
+    #used to back convert to words from index
+    ix_to_word = {} 
+    for key, value in word_to_ix.items(): 
+        if value in ix_to_word: 
+            ix_to_word[value].append(key) 
+            print(value)
+            print(key)
+            print(ix_to_word[value])
+        else: 
+            ix_to_word[value]=[key] 
+
+    #used to back convert to words from prop
+    ix_to_prop = {} 
+    for key, value in prop_to_ix.items(): 
+        if value in ix_to_prop: 
+            ix_to_prop[value].append(key) 
+            print(value)
+            print(key)
+            print(ix_to_prop[value])
+        else: 
+            ix_to_prop[value]=[key] 
+
+    #used to back convert to words from prop
+    ix_to_ner = {} 
+    for key, value in ner_to_ix.items(): 
+        if value in ix_to_ner: 
+            ix_to_ner[value].append(key) 
+            print(value)
+            print(key)
+            print(ix_to_ner[value])
+        else: 
+            ix_to_ner[value]=[key] 
+
+    #used to back convert to words from pos
+    ix_to_pos = {} 
+    for key, value in pos_to_ix.items(): 
+        if value in ix_to_pos: 
+            ix_to_pos[value].append(key) 
+            print(value)
+            print(key)
+            print(ix_to_pos[value])
+        else: 
+            ix_to_pos[value]=[key] 
+
+    revindicies = defaultdict()
+    revindicies = {'ix_to_word': ix_to_word,
+                'ix_to_prop':ix_to_prop,
+                'ix_to_ner':ix_to_ner,
+                'ix_to_pos':ix_to_pos}
+
+    return vectorizeddata, indicies, revindicies, wordvocab
+
+def build_weights_matrix(vocab, embedding_file, embedding_dim):
+    """
+    used to apply pretrained embeddigns to vocabulary
+    """
+    print("--- Building Pretrained Embedding Index  --- %s seconds ---" % (round((time.time() - start_time),2)))
+    words = []
+    embeddings_index = {}
+    with open (embedding_file, encoding="utf8") as f:
+        for line in f:
+            values = line.split()
+            word = values[0]
+            words.append(word)
+            embedding = np.asarray(values[1:], dtype='float32')
+            embeddings_index[word] = embedding
+
+    
+    matrix_len = len(vocab)
+    weights_matrix = np.zeros((matrix_len, embedding_dim)) # 200 is depth of embedding matrix
+    words_found = 0
+    words_not_found = 0
+    for i, word in enumerate(vocab):
+        try:
+            weights_matrix[i] = embeddings_index[word] #assign the pretrained embedding
+            words_found += 1
+        
+        except KeyError:
+            weights_matrix[i] = np.random.normal(scale=0.6, size=(embedding_dim,)) #randomize out of vocabulary words
+            words_not_found += 1
+            
+    print("{:.2f}% ({}/{}) of the vocabulary were in the pre-trained embedding.".format((words_found/len(vocab))*100,words_found,len(vocab)))
+    return torch.from_numpy(weights_matrix)
 
 if __name__ == "__main__":
     main()
